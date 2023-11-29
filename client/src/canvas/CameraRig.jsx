4e67b354a0b70b574c11f941537e7091e8713528
Sairoden <odingandarosa@gmail.com>
2023-11-29 15:29:01 +0800

    Fix CameraRig Canvas

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -14,7 +14,7 @@ function CameraRig({ children }) {
     const isBreakpoint = window.innerWidth <= 1260;
     const isMobile = window.innerWidth <= 600;
 
-    // Set the initial positon of the model
+    // set the initial position of the model
     let targetPosition = [-0.4, 0, 2];
     if (snap.intro) {
       if (isBreakpoint) targetPosition = [0, 0, 2];
@@ -24,17 +24,16 @@ function CameraRig({ children }) {
       else targetPosition = [0, 0, 2];
     }
 
-    // Set model camera position
+    // set model camera position
     easing.damp3(state.camera.position, targetPosition, 0.25, delta);
 
-    if (snap.intro)
-      // Set the model rotation smoothly
-      easing.dampE(
-        group.current.rotation,
-        [state.pointer.y / 10, -state.pointer.x / 5, 0],
-        0.25,
-        delta
-      );
+    // set the model rotation smoothly
+    easing.dampE(
+      group.current.rotation,
+      [state.pointer.y / 10, -state.pointer.x / 5, 0],
+      0.25,
+      delta
+    );
   });
 
   return <group ref={group}>{children}</group>;
